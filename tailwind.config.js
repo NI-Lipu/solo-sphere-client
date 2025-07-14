c4f1e5dbd4e004d091564ebd08a3b9420f33f3c5
NI Lipu <nilipu500@gmail.com>
2025-07-14 08:00:27 +0600

    feat:Handle Add job form submission And post the submission data to the server by using Axios

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui'
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        lato: "'Lato', sans-serif",
+   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+   theme: {
+      extend: {
+         fontFamily: {
+            lato: "'Lato', sans-serif",
+         },
       },
-    },
-  },
-  plugins: [require('daisyui')],
+   },
+   plugins: [daisyui],
 }
